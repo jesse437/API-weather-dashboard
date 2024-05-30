6e16afa88d7f8e3c05adbfcb299b180cfa23c8bf
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-05-30 06:40:20 -0700

    Updated http to https to handle errors in deploed application

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const weatherImage = document.querySelector(".weatherIcon");
 const temp = document.querySelector(".temp");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
-const cardDeck = document.querySelector('.card-deck');
-
-
+const cardDeck = document.querySelector(".card-deck");
 
 var today = new Date();
 const [day, month, year] = [
@@ -33,48 +31,50 @@ searchBtn.addEventListener("click", function () {
   }
   localStorage.setItem("cities", JSON.stringify(cityList));
   localStorage.setItem("currentCity", JSON.stringify(city_name));
-  
-    displayWeather(city_name);
-    displayFiveDayForecast(city_name);
-    displaySearchedCities();
-    historyCities();
+
+  displayWeather(city_name);
+  displayFiveDayForecast(city_name);
+  displaySearchedCities();
+  historyCities();
 });
 
-function localCityList(){
-  const savedData = JSON.parse(localStorage.getItem('cities'));
-  if (savedData != null){
+function localCityList() {
+  const savedData = JSON.parse(localStorage.getItem("cities"));
+  if (savedData != null) {
     cityList = savedData;
     displaySearchedCities();
-  }else{
+  } else {
     console.log("No data in searched city", cityList);
-    localStorage.setItem('cities', JSON.stringify(cityList));
+    localStorage.setItem("cities", JSON.stringify(cityList));
   }
 }
 function displaySearchedCities() {
-  const city_list = document.querySelector('#cityList');
-  city_list.innerHTML="";
-  const savedData = JSON.parse(localStorage.getItem('cities'));
-  if (savedData != null){
-    if (savedData.length > 0){
-      for (let i=0; i<savedData.length; i++){
-        var searchedCity = document.createElement('button');
-        searchedCity.classList.add("btn", "btn-secondary", "btn-lg", "btn-block", "city");
+  const city_list = document.querySelector("#cityList");
+  city_list.innerHTML = "";
+  const savedData = JSON.parse(localStorage.getItem("cities"));
+  if (savedData != null) {
+    if (savedData.length > 0) {
+      for (let i = 0; i < savedData.length; i++) {
+        var searchedCity = document.createElement("button");
+        searchedCity.classList.add(
+          "btn",
+          "btn-secondary",
+          "btn-lg",
+          "btn-block",
+          "city"
+        );
         searchedCity.setAttribute("data-name", savedData[i]);
-        searchedCity.innerHTML=savedData[i];
+        searchedCity.innerHTML = savedData[i];
         city_list.prepend(searchedCity);
-     }
+      }
     }
   }
-
 }
-
-
-
 
 function displayWeather(city_name) {
   city_name = city_name.toLowerCase();
   let weatherData = fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=5fed619e7304f60a938b70a219a457d0`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=5fed619e7304f60a938b70a219a457d0`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -94,7 +94,7 @@ function displayWeather(city_name) {
           var weatherIcon = data["list"][0]["weather"][0]["icon"];
           weatherImage.setAttribute(
             "src",
-            `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
           );
           // Current Temperature
           var tempValue = data["list"][0]["main"]["temp"];
@@ -112,12 +112,12 @@ function displayWeather(city_name) {
           humidity.innerHTML = `Humidity: ${humidityValue} %`;
         });
     });
-    historyCities()
+  historyCities();
 }
 
 function displayFiveDayForecast(city_name) {
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=5fed619e7304f60a938b70a219a457d0`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=5fed619e7304f60a938b70a219a457d0`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -129,48 +129,46 @@ function displayFiveDayForecast(city_name) {
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5fed619e7304f60a938b70a219a457d0&units=imperial`
       )
         .then((response) => response.json())
-          .then((data) => {
-            console.log(data['cnt']);
-            cardDeck.innerHTML=""
-              for (let i = 0; i < data['cnt']; i = i + 8){
-                    dt_txt = data['list'][i]['dt_txt'].slice(0, 10).split('-');
-                    var [yyyy, mm, dd] = dt_txt;
-                  var weatherIcon = data["list"][i]["weather"][0]["icon"];
-                  var tempValue = data["list"][i]["main"]["temp"];
-                  var windValue = data["list"][i]["wind"]["speed"];
-                  var humidityValue = data["list"][0]["main"]["humidity"];
-                    const template = `
+        .then((data) => {
+          console.log(data["cnt"]);
+          cardDeck.innerHTML = "";
+          for (let i = 0; i < data["cnt"]; i = i + 8) {
+            dt_txt = data["list"][i]["dt_txt"].slice(0, 10).split("-");
+            var [yyyy, mm, dd] = dt_txt;
+            var weatherIcon = data["list"][i]["weather"][0]["icon"];
+            var tempValue = data["list"][i]["main"]["temp"];
+            var windValue = data["list"][i]["wind"]["speed"];
+            var humidityValue = data["list"][0]["main"]["humidity"];
+            const template = `
                     <div class="card-body">
                         <h5 class="card-title">${mm}/${dd}/${yyyy}</h5>
-                        <img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
+                        <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png">
                         <p class="card-text">Temp: ${tempValue} °F</p>
                         <p class="card-text">Wind: ${windValue} MPH</p>
                         <p class="card-text">Humidity: ${humidityValue} %</p>
                     </div>
-                `
-                  var card = document.createElement('div');
-                  card.classList.add('card');
-                  card.innerHTML = template;
+                `;
+            var card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = template;
 
-                  cardDeck.append(card);
-                }
-              
+            cardDeck.append(card);
+          }
         });
     });
 }
 
 function historyCities() {
-  const storedCitiesList = document.querySelectorAll('.city');
-storedCitiesList.forEach(storedCity => {
-  storedCity.addEventListener('click', function(event){
-    event.preventDefault();
-    clickedCity = storedCity.getAttribute('data-name');
-    displayWeather(clickedCity);
-    displayFiveDayForecast(clickedCity);
-  })
-});
+  const storedCitiesList = document.querySelectorAll(".city");
+  storedCitiesList.forEach((storedCity) => {
+    storedCity.addEventListener("click", function (event) {
+      event.preventDefault();
+      clickedCity = storedCity.getAttribute("data-name");
+      displayWeather(clickedCity);
+      displayFiveDayForecast(clickedCity);
+    });
+  });
 }
-
 
 displayWeather("San Diego");
 displayFiveDayForecast("San Diego");
